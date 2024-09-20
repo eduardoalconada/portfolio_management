315aa2ddbd209da6b47c2ec5b65678b1f92a5a82
eduardoalconada <ed.alconada@gmail.com>
2024-09-20 16:44:50 +0200

    structured liquidityUPD file

--- liquidityUPD.py
+++ liquidityUPD.py
@@ -4,26 +4,30 @@ import time
 import requests
 from calcEUR2USD import pairEUR2USD
 
+path_liq = 'components/liquidity.json'
+
 def update_liquidity(liq_eur):
 
     liq_usd = liq_eur * pairEUR2USD()
 
-    data["LIQ-USD"] = liq_usd
+    data["LIQ-USD"] = round(liq_usd, 2)
 
-    with open('components/liquidity.json', 'w') as f:
+    with open(path_liq, 'w') as f:
         json.dump(data, f, indent=4)
 
-if __name__ == "__main__":
-
+def open_liq_json():
     try:
-        with open('components/liquidity.json', 'r') as f:
+        with open(path_liq, 'r') as f:
             data = json.load(f)
     except FileNotFoundError:
         data = {"LIQ-EUR": 0, "LIQ-USD": 0}
 
+    return data
 
+def update_amount(data): 
 
     upd_amount = input("Do you want to update the amount of liquidity? (y/n)").lower()
+
     if upd_amount == "y":
         red_inc = input("Do you want to increase or reduce the amount? (i/r)").lower()
         amount = int(input("Enter the amount: "))
@@ -35,5 +39,10 @@ if __name__ == "__main__":
         with open('components/liquidity.json', 'w') as f:
             json.dump(data, f, indent=4)
 
+if __name__ == "__main__":
+    
+    data = open_liq_json()
+
+    update_amount(data)
 
     update_liquidity(data["LIQ-EUR"])
